a557f708f7ff555b7c74acfa8cc513f334f33197
Surya M <surya.m@phonepe.com>
2024-03-28 20:32:21 +0530

    [chore]: import fix

--- app/services/auth/auth_bearer.py
+++ app/services/auth/auth_bearer.py
@@ -1,5 +1,7 @@
 from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 
 from .auth_handler import decodeJWT
 from fastapi import FastAPI, Body, Depends
@@ -8,8 +10,32 @@ from sqlalchemy.orm.session import Session
 from app.models.user import User
 import json
 
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
 
-def get_current_user(token: str,db: Session = Depends(get_session)):
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtoken: str) -> bool:
+        isTokenValid: bool = False
+        try:
+            payload = decodeJWT(jwtoken)
+        except:
+            payload = None
+        if payload:
+            isTokenValid = True
+        return isTokenValid
+
+def get_current_user(token: str = Depends(JWTBearer()),db: Session = Depends(get_session)):
     decodedData,state = decodeJWT(token)
     details = decodedData["sub"]
     user_data = db.query(User).filter(User.email == details.get("email")).first() # type: ignore
